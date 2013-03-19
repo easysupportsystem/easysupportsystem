616629aaadbb0fb319b939c7fc12982eca1f67b2
Marcus Hann <marcushann@epikbeats.co.uk>
2013-03-19 21:19:54 +0000

    Added proper nav structure
    Added proper nav text and added hero unit.

--- index.php
+++ index.php
@@ -9,19 +9,32 @@ if (isset($_SESSION['username'])){
         <title>Easy Support System</title>
         <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css" />
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+        <script type="text/javascript" src="js/bootstrap.js"></script>
     </head>
     <body>
         <div class="navbar navbar-fixed-top">
             <div class="navbar-inner">
                 <div class="container">
-                    <a class="brand" href="#">Title</a>
+                    <a class="brand" href="#">Easy Support System</a>
                     <ul class="nav">
                         <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Tickets</a></li>
+                        <li><a href="#">Wiki</a></li>
+                        <li><a href="#">Forum</a></li>
+                        <li><a href="#">Login</a></li>
                     </ul>
                 </div>
             </div>
+        </div>
+        
+        <div class="container">
+            <div class="hero-unit">
+                <h1>Easy Support System</h1>
+            </div>
+        </div>
+        
+        <div class="container">
+            
         </div>
     </body>
 </html>
